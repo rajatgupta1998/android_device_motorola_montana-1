57c545ab4113202bc2c72fb93347fb2235ca6c59
Abhishek Aggarwal <TheScarastic>
2019-02-17 14:40:03 +0530

    montana: MotoActions: Add DiracSound backend

--- MotoActions/src/com/moto/actions/BootCompletedReceiver.java
+++ MotoActions/src/com/moto/actions/BootCompletedReceiver.java
@@ -27,7 +27,7 @@ import android.content.ServiceConnection;
 import android.os.IBinder;
 import android.util.Log;
 
-import com.moto.actions.util.FileUtils;
+import com.moto.actions.dirac.DiracUtils;
 import com.moto.actions.actions.Constants;
 import com.moto.actions.ServiceWrapper.LocalBinder;
 
@@ -47,7 +47,8 @@ public class BootCompletedReceiver extends BroadcastReceiver {
         }
 
         context.startService(new Intent(context, ServiceWrapper.class));
-    }
+        DiracUtils.initialize();
+   }
 
     private ServiceConnection serviceConnection = new ServiceConnection() {
         @Override
