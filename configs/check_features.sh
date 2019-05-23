0c694b503ac03a7d4c91adf648f152d903787bfc
AsD Monio <asdmonio@gmail.com>
2019-05-23 16:06:27 +0200

    check_features: Clean up NFC removal logic
    Combine the sku checks, remove redundant ones
    
    Change-Id: I9f72d39d65102296704b6305ad738b4b8670e32f

--- configs/check_features.sh
+++ configs/check_features.sh
@@ -2,32 +2,13 @@
 
 sku=`getprop ro.boot.hardware.sku`
 
-if [ "$sku" = "XT1792" ]; then
-    # XT1792 doesn't have NFC
+if [ "$sku" = "XT1792" ] || [ "$sku" == "XT1795" ]; then
+    # XT1792 and XT1795 don't have NFC
     rm /vendor/etc/permissions/android.hardware.nfc.xml
     rm /vendor/etc/permissions/android.hardware.nfc.hce.xml
     rm /vendor/etc/permissions/com.android.nfc_extras.xml
     rm -r /vendor/app/NfcNci
 fi
-
-if [ "$sku" == "XT1792" ]; then
-    # XT1792 doesn't have NFC
-    remove_nfc
-fi
-
-if [ "$sku" == "XT1795" ]; then
-    # XT1795 doesn't have NFC
-    rm /vendor/etc/permissions/android.hardware.nfc.xml
-    rm /vendor/etc/permissions/android.hardware.nfc.hce.xml
-    rm /vendor/etc/permissions/com.android.nfc_extras.xml
-    rm -r /vendor/app/NfcNci
-fi
-
-if [ "$sku" == "XT1795" ]; then
-    # XT1795 doesn't have NFC
-    remove_nfc
-fi
-
 # Remove DTB stuff
 rm /vendor/etc/permissions/com.motorola.hardware.dtv.xml
 rm /vendor/etc/permissions/mot_dtv_permissions.xml
