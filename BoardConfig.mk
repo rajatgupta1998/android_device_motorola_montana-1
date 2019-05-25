d39d4e3ee00b99759d0918b0956419751172ee05
AsD Monio <asdmonio@gmail.com>
2019-05-25 11:02:44 +0200

    Revert "montana: Build the shims"
    This reverts commit 47fe4fb57877c7f6c317d1042544193cef81ab03.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -228,9 +228,6 @@ VENDOR_SECURITY_PATCH := 2019-02-01
 include device/qcom/sepolicy/sepolicy.mk
 #BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
-# Shims
-TARGET_USE_SHIMS := true
-
 # Thermal
 USE_DEVICE_SPECIFIC_THERMAL := true
 
