79318b970650ad218bdd572a56e47b4fb8a6db53
AsdMonio <asdmonio@gmail.com>
2019-06-17 18:01:12 +0300

    check_features: cleanup NFC removal logic
    Signed-off-by: JarlPenguin <jarlpenguin@outlook.com>
    Change-Id: Id6385498063309046e842f7be2285f4b8bd2c587

--- configs/check_features.sh
+++ configs/check_features.sh
@@ -1,7 +1,6 @@
 #!/sbin/sh
 
 sku=`getprop ro.boot.hardware.sku`
-device=`getprop ro.boot.device`
 
 remove_nfc() {
     rm /system/vendor/etc/permissions/android.hardware.nfc.xml
@@ -10,32 +9,22 @@ remove_nfc() {
     rm -r /system/app/NfcNci
 }
 
-if [ "$sku" == "XT1687" ] || [ "$device" = "cedric" ]; then
-    # XT1687 and cedric don't have NFC chips
+if [ "$sku" == "XT1790" ]  || [ "$sku" == "XT1792" ] || [ "$sku" == "XT1795" ]; then
+    # XT1790, XT1792 and XT1795 don't have NFC
     remove_nfc
 fi
 
-if [ "$device" = "montana" ]; then
-    if [ "$radio" != "EMEA" ] && [ "$radio" != "APAC" ]; then
-        # On montana only EMEA and APAC radio models have NFC
-        remove_nfc
-    fi
-fi
+# Remove DTB stuff
+rm /system/vendor/etc/permissions/com.motorola.hardware.dtv.xml
+rm /system/vendor/etc/permissions/mot_dtv_permissions.xml
+rm /system/vendor/lib/libdtvtuner.so
+rm /system/vendor/lib64/libdtvtuner.so
+rm /system/vendor/lib/libdtvhal.so
+rm /system/vendor/lib64/libdtvhal.so
+rm -r /system/vendor/app/DTVPlayer
+rm -r /system/vendor/app/DTVService
 
-if [ "$sku" != "XT1687" ]; then
-    # Only XT1687 variant got a compass
+if [ "$sku" != "XT1790" ] || [ "$sku" != "XT1792" ] || [ "$sku" != "XT1795" ]; then
+    # XT1790,XT1792 and XT1795 have a compass
     rm /system/vendor/etc/permissions/android.hardware.sensor.compass.xml
 fi
-
-if [ "$sku" != "XT1683" ]; then
-    # Only XT1683 has DTV support
-    rm /system/vendor/etc/permissions/com.motorola.hardware.dtv.xml
-    rm /system/vendor/etc/permissions/mot_dtv_permissions.xml
-    rm /system/vendor/lib/libdtvtuner.so
-    rm /system/vendor/lib64/libdtvtuner.so
-    rm /system/vendor/lib/libdtvhal.so
-    rm /system/vendor/lib64/libdtvhal.so
-    rm -r /system/vendor/app/DTVPlayer
-    rm -r /system/vendor/app/DTVService
-fi
-
