51f2886e475dc16aa7f7d8ffcb3adec9b0aa2da6
JarlPenguin <jarlpenguin@outlook.com>
2019-07-18 11:26:06 +0300

    montana: check_features: remove DTV removal script
    * We don't ship it anymore
    
    Signed-off-by: JarlPenguin <jarlpenguin@outlook.com>

--- configs/check_features.sh
+++ configs/check_features.sh
@@ -14,16 +14,6 @@ if [ "$sku" == "XT1790" ]  || [ "$sku" == "XT1792" ] || [ "$sku" == "XT1795" ]; 
     remove_nfc
 fi
 
-# Remove DTB stuff
-rm /system/vendor/etc/permissions/com.motorola.hardware.dtv.xml
-rm /system/vendor/etc/permissions/mot_dtv_permissions.xml
-rm /system/vendor/lib/libdtvtuner.so
-rm /system/vendor/lib64/libdtvtuner.so
-rm /system/vendor/lib/libdtvhal.so
-rm /system/vendor/lib64/libdtvhal.so
-rm -r /system/vendor/app/DTVPlayer
-rm -r /system/vendor/app/DTVService
-
 if [ "$sku" != "XT1790" ] || [ "$sku" != "XT1792" ] || [ "$sku" != "XT1795" ]; then
     # XT1790,XT1792 and XT1795 have a compass
     rm /system/vendor/etc/permissions/android.hardware.sensor.compass.xml
