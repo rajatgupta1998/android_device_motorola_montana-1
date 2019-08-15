da79b75f5e36641149e77efa620784798cb082ce
BarryBlackCat <everydayonechoice@gmail.com>
2019-08-15 15:53:22 +0000

    montana: remove PRODUCT_SYSTEM_PROPERTY_BLACKLIST

--- aosp_montana.mk
+++ aosp_montana.mk
@@ -49,9 +49,5 @@ PRODUCT_GMS_CLIENTID_BASE := android-motorola
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="montana_retail-user 8.1.0 OPPS28.65-37-7-4 1dd5 release-keys"
 
-PRODUCT_SYSTEM_PROPERTY_BLACKLIST := \
-    ro.product.model \
-    ro.vendor.product.model
-
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
 BUILD_FINGERPRINT := motorola/montana_retail/montana:8.1.0/OPPS28.65-37-7-4/1dd5:user/release-keys
